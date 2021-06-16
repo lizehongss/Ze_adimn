bed0a131738f96a46a658775268db363265fccaa
lizehong <13600484164@163.com>
2021-06-16 20:26:48 +0800

    feat: 添加mock模块

---
+++ mock/_util.ts
@@ -0,0 +1,8 @@
+export function resultSucces<T = Recordable>(result: T, { message = 'ok' } = {}) {
+  return {
+    code: 0,
+    result,
+    message,
+    type: 'success'
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import { UserConfigExport, ConfigEnv } from 'vite'
+import { viteMockServe } from 'vite-plugin-mock'
 import vue from '@vitejs/plugin-vue'
 // const { resolve } = require('path')
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ command }: ConfigEnv): UserConfigExport => {
+  const isBuild = command === 'build';
+  return{
+    plugins: [
+      vue(),
+      viteMockServe({
+        ignore: /^\_/,
+        mockPath: 'mock',
+        localEnabled: !isBuild,
+        prodEnabled: isBuild,
+        injectCode: `
+          import { setupProdMockServer } from './mock/_createProductionServer';
+    
+          setupProdMockServer();
+          `,
+      })
+    ]
+  }
+}
